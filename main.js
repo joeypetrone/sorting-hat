e2fef733c6bbd3e389aa2a5566b4efe13012108e
Joey Petrone <joey.petrone@gmail.com>
2020-01-30 19:10:42 -0600

    Added array and functions for student sorting

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+// STUDENT ARRAY
+studentArray = [];
+
+// HOUSE ARRAY 
+houseArray = [
+    'Gryffindor',
+    'Hufflepuff',
+    'Ravenclaw',
+    'Slytherin'
+];
+
 // PRINT TO DOM
 const printToDom = (divId, textToPrint) => {
     const selectedId = document.getElementById(divId);
@@ -10,23 +21,31 @@ const formBuider = () => {
     domString += '<form>';
     domString += '  <div class="form-group">';
     domString += '    <label>Lets find your Hogarts House, enter your name to be sorted!</label>';
-    domString += '          <input class="form-control" placeholder="Type your name here">';
+    domString += '          <input id="student-input" class="form-control" placeholder="Type your name here">';
     domString += '    <small class="form-text text-muted">We\'ll find the most suitable house for you.</small>';
     domString += '  </div>';
-    domString += '  <button type="submit" class="btn btn-primary mb-2">Submit</button>';
+    domString += '  <button id="submit-btn" class="btn btn-primary mb-2">Submit</button>';
     domString += '</form>';
 
     printToDom('student-form', domString);
+    document.getElementById('submit-btn').addEventListener('click', houseSorter );
 };
 
-// FORM DISPLAY FUNCTION
-const displayForm = () => {
-    alert('Join Howarts, one moment...');
+// STUDENT ARRAY BUILDER
+const studentArrayBuilder = () => {
+    studentArray.push(document.getElementById('student-input').value);
+};
+
+// HOUSE SORTER
+const houseSorter = () => {
+    let newHouse = '';
+    newHouse = houseArray[Math.floor(Math.random() * houseArray.length)];
+    console.log(newHouse);
 };
 
 // EVENTS
 const events = () => {
-    document.getElementById('join').addEventListener('click', formBuider );
+    document.getElementById('join-btn').addEventListener('click', formBuider );
 };
 
 // INITIAL FUNCTION
