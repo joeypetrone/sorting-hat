5924e2694f82f1a2e1fd1d5a34d565624db65401
Joey Petrone <joey.petrone@gmail.com>
2020-01-30 18:27:12 -0600

    Added bootstrap form

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-console.log('Sorting Time!');
+// PRINT TO DOM
+const printToDom = (divId, textToPrint) => {
+    const selectedId = document.getElementById(divId);
+    selectedId.innerHTML = textToPrint;
+};
 
-// CLICK FUNCTION
-const clickedButton = () => {
-    alert('CLICK!!!');
+// FORM BUILDER 
+const formBuider = () => {
+    let domString = '';
+    domString += '<form>';
+    domString += '  <div class="form-group">';
+    domString += '    <label>Lets find your Hogarts House, enter your name to be sorted!</label>';
+    domString += '          <input class="form-control" placeholder="Type your name here">';
+    domString += '    <small class="form-text text-muted">We\'ll find the most suitable house for you.</small>';
+    domString += '  </div>';
+    domString += '  <button type="submit" class="btn btn-primary mb-2">Submit</button>';
+    domString += '</form>';
+
+    printToDom('student-form', domString);
+};
+
+// FORM DISPLAY FUNCTION
+const displayForm = () => {
+    alert('Join Howarts, one moment...');
 };
 
 // EVENTS
 const events = () => {
-    document.getElementById('click-button').addEventListener('click', clickedButton )
+    document.getElementById('join').addEventListener('click', formBuider );
 };
 
 // INITIAL FUNCTION
